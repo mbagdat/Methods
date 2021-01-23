82a311a3a482afe4d709172fc91c4f06444cc4ea
mutebr <bagdatmutlu@gmail.com>
2021-01-23 16:22:33 +0300

    20 Ocak Ders Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string [] isimler=new string[] {"mutlu","ebru",""}
+
+            List<string> isimler = new List<string> { "mutlu", "ebru", "canan", "necati" };
+            isimler.Add("Hasan");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 1;
+            product.CategoryId = 2;
+            product.ProductName = "Masa";
+            product.UnitPrice = 500;
+            product.UnitsInStock = 3;
+
+
+            ProductService productService = new ProductService();
+            productService.Add(product);
+            //Console.WriteLine(product.ProductName);
+        }
+    }
+}
